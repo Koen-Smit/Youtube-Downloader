007dc104c887adec29531cf01ee5ba5b8ab3e0ae
KerrieMeister96 <koensmit13@gmail.com>
2024-02-25 20:25:07 +0100

    Cleanup + audio fix

--- downloader/download.py
+++ downloader/download.py
@@ -1,18 +1,15 @@
-# Youtube video downloader
-
-# import libraries
+# ========================================== Youtube video downloader ==========================================
+# import libraries + clear the terminal
 import os
+os.system('cls' if os.name == 'nt' else 'clear')
 import sys
 import time
 import subprocess
 from pytube import YouTube
 from pytube.cli import on_progress
 
-#clear the console 
-#os.system('cls' if os.name == 'nt' else 'clear')
-
 # function to print progress bar
-def progress_bar(stream, chunk, file_handle, bytes_remaining):
+def progress_bar(stream, bytes_remaining):
     contentSize = stream.filesize
     size = contentSize
     p = 0
@@ -26,12 +23,14 @@ def percent(tem, total):
     perc = (float(tem) / float(total)) * float(100)
     return perc
 
-# function to download video + the audio
+# function to download video with audio
 def download_video(url, path):
     # download the video
     yt = YouTube(url, on_progress_callback=on_progress)
-    yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download(path)
+    stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+    stream.download(path)
     print("Download completed")
+
 
 # function call the download function
 def download(url, path, audio=True):
@@ -51,14 +50,12 @@ def download(url, path, audio=True):
         if sys.platform == "win32":
             os.startfile(f"{path}/{url.split('=')[1]}.mp4")
         else:
-            opener = "open" if sys.platform == "darwin" else "xdg-open"
-            subprocess.call([opener, f"{path}/{url.split('=')[1]}.mp4"])
+            print("Can't open the file")
 
 # main function
 def main():
     # ask for the url, path and audio
-    url = input("Enter the video url: ")
-    #https://www.youtube.com/watch?v=UmtmkMrFFRI TEST URL
+    url = input("Enter the video url: ") #https://www.youtube.com/watch?v=UmtmkMrFFRI TEST URL
     path = "downloads"
     audio = True if input("Do you want to download audio? (y/n): ").lower() == "y" else False
 
